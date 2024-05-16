2150a3ef7b0b646d2c3c2d2455e7131a3cfc735e
Igor Kevin <igor_kevin@hotmail.com>
2024-05-16 17:10:38 -0300

    Lista com rendering agora, podendo ser moldado dinamicamente

--- my-react-app/src/components/ListGroup.tsx
+++ my-react-app/src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 function ListGroup() {
+  const items = [
+    "São Paulo",
+    "Rio de Janeiro",
+    "Bahia",
+    "Tocantins",
+    "Espirito Santo",
+  ];
+
   return (
     <>
       <h1>List </h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </>
   );
